b5bb75d7f4b5f42cfe9e6745d9e303b533d91b33
Reaper <max.m.roy90@gmail.com>
2022-02-22 00:46:06 +0000

    Update parseTree.spec.js

--- parseTree.spec.js
+++ parseTree.spec.js
@@ -1,102 +1,132 @@
-parseTree = require('../modules/parseTree');
+KeySwapper = require('../modules/swapKeys');
 
 describe('Testing Parse tree to swap keys', () => {
 
     const key2swap = {
-        a: 'aa'
+        A: 'aa'
     };
 
     const keys2swap = {
-        a: 'aa',
-        b: 'bb',
+        A: 'aa',
+        B: 'bb',
     };
 
     test('primative value that is different from key to be swapped should not change', () => {
         data = 'b';
-        swapped = parseTree.parseTree(key2swap, data);
+
+        keySwapper = new KeySwapper(data, key2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(data);
     });
 
     test('primative value that is same as key to be swapped should not change', () => {
         data = 'a';
-        swapped = parseTree.parseTree(key2swap, data);
+
+        keySwapper = new KeySwapper(data, key2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(data);
     });
 
-    test('empty array should not change', () => {
+    test('empty array should return empty array', () => {
         data = [];
-        swapped = parseTree.parseTree(key2swap, data);
-        expect(swapped).toEqual(data);
+        expected = [];
+        keySwapper = new KeySwapper(data, key2swap);
+        swapped = keySwapper.swap();
+
+        expect(swapped).toEqual(expected);
     });
 
     test('array containing primative containing key to be swapped should not change', () => {
         data = ['a'];
-        swapped = parseTree.parseTree(key2swap, data);
+
+        keySwapper = new KeySwapper(data, key2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(data);
     });
 
     test('array containing multiple primatives with key values to be swapped should not change', () => {
         data = ['a', 'a', 'b'];
-        swapped = parseTree.parseTree(key2swap, data);
+
+        keySwapper = new KeySwapper(data, key2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(data);
     });
 
     test('Object with key to be swapped should be changed', () => {
-        data = { a: "a" };
+        data = { A: "a" };
         expected = { aa: "a" };
-        swapped = parseTree.parseTree(key2swap, data);
+
+        keySwapper = new KeySwapper(data, key2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(expected);
     });
 
     test('Object with multiple matching keys to be swapped should be changed', () => {
-        data = { a: "a" , b: "b" };
+        data = { A: "a" , B: "b" };
         expected = { aa: "a" , bb: "b" };
-        swapped = parseTree.parseTree(keys2swap, data);
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(expected);
     });
 
     test('Only matching keys to be swapped should be changed', () => {
-        data = { a: "a" , b: "b", c:'c' };
+        data = { A: "a" , B: "b", C:'c' };
         expected = { aa: "a" , bb: "b", c:'c' };
-        swapped = parseTree.parseTree(keys2swap, data);
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(expected);
     });
 
     test('nested objects in lists should have keys changed', () => {
         data = [
-            { a: "a1" , b: "b1", c: 'c1'},
-            { a: "a2" , b: "b2", c: 'c2'},
+            { A: "a1" , B: "b1", C: 'c1'},
+            { A: "a2" , B: "b2", C: 'c2'},
         ];
         expected = [
             { aa: "a1" , bb: "b1", c: 'c1'},
             { aa: "a2" , bb: "b2", c: 'c2'},
         ];
-        swapped = parseTree.parseTree(keys2swap, data);
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(expected);
     });
 
     test('nested objects in objects should have keys changed', () => {
         data = {
-            a: { a: "a1" , b: "b1", c: 'c1'},
-            b: { a: "a2" , b: "b2", c: 'c2'},
+            A: { A: "a1" , B: "b1", C: 'c1'},
+            B: { A: "a2" , B: "b2", C: 'c2'},
         };
         expected = {
             aa: { aa: "a1" , bb: "b1", c: 'c1'},
             bb: { aa: "a2" , bb: "b2", c: 'c2'},
         };
-        swapped = parseTree.parseTree(keys2swap, data);
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(expected);
     });
 
     test('nested objects containing objetcs in arrays should have keys changed', () => {
         data = { 
-            a: [
-                { a: "a1" , b: "b1", c: 'c1'},
-                { a: "a2" , b: "b2", c: 'c2'},
+            A: [
+                { A: "a1" , B: "b1", C: 'c1'},
+                { A: "a2" , B: "b2", C: 'c2'},
             ],
-            b: [
-                { a: "a1" , b: "b1", c: 'c1'},
-                { a: "a2" , b: "b2", c: 'c2'},
+            B: [
+                { A: "a1" , B: "b1", C: 'c1'},
+                { A: "a2" , B: "b2", C: 'c2'},
             ],
         },
         expected = { 
@@ -109,19 +139,22 @@ describe('Testing Parse tree to swap keys', () => {
                 { aa: "a2" , bb: "b2", c: 'c2'},
             ],
         },
-        swapped = parseTree.parseTree(keys2swap, data);
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(expected);
     });
 
     test('list of objects in objects should have keys changed', () => {
         data = [
             { 
-                a: { a: "a1" , b: "b1", c: 'c1'},
-                b: { a: "a2" , b: "b2", c: 'c2'},
+                A: { A: "a1" , B: "b1", C: 'c1'},
+                B: { A: "a2" , B: "b2", C: 'c2'},
             },
             { 
-                a: { a: "a1" , b: "b1", c: 'c1'},
-                b: { a: "a2" , b: "b2", c: 'c2'},
+                A: { A: "a1" , B: "b1", C: 'c1'},
+                B: { A: "a2" , B: "b2", C: 'c2'},
             },
         ],
         expected = [
@@ -134,23 +167,124 @@ describe('Testing Parse tree to swap keys', () => {
                 bb: { aa: "a2" , bb: "b2", c: 'c2'},
             },
         ],
-        swapped = parseTree.parseTree(keys2swap, data);
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
+        expect(swapped).toEqual(expected);
+    });
+
+    test('all keys should be made lowercase', () => {
+        data = [
+            { 
+                A: { A: "a1" , B: "b1", C: 'c1'},
+                B: { A: "a2" , B: "b2", C: 'c2'},
+            },
+            { 
+                A: { A: "a1" , B: "b1", C: 'c1'},
+                B: { A: "a2" , B: "b2", C: 'c2'},
+            },
+        ],
+        expected = [
+            { 
+                aa: { aa: "a1" , bb: "b1", c: 'c1'},
+                bb: { aa: "a2" , bb: "b2", c: 'c2'},
+            },
+            { 
+                aa: { aa: "a1" , bb: "b1", c: 'c1'},
+                bb: { aa: "a2" , bb: "b2", c: 'c2'},
+            },
+        ],
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
         expect(swapped).toEqual(expected);
     });
 });
 
-describe('Testing keys to swap can be reversed', () => {
-    test('Testing keys to swap can be reveresed', () => {
-        swap = { a: '1' };
-        reversed = {'1': 'a'};
+describe('Testing keys are non-capitalised and preserve camalCase', () => {
+    const keys2swap = {
+        'PeachPie': 'poachedPear'
+    };
 
-        expect(reversed).toEqual(parseTree.reverseKeys(swap));
+    test('keys that are not in keys2swap are non capitalised', () => {
+        data = {
+            'ApplePie': 1,
+            'PizzaPie': 2,
+        };
+        expected = {
+            'applePie': 1,
+            'pizzaPie': 2,
+        };
+
+        keySwapper = new KeySwapper(data, {});
+        swapped = keySwapper.swap();
+
+        expect(swapped).toEqual(expected);
     });
 
-    test('Testing multiple keys to swap can be reveresed', () => {
-        swap = { a: '1' , b: '2'};
-        reversed = {'1': 'a', '2': 'b'};
+    test('nested camalCase keys that are not in keys2swap are non capitalised', () => {
+        data = {
+            'ApplePie': {
+                'PeachPie': 1,
+            },
+            'PizzaPie': 2,
+        };
+        expected = {
+            'applePie': {
+                'poachedPear': 1
+            },
+            'pizzaPie': 2,
+        };
 
-        expect(reversed).toEqual(parseTree.reverseKeys(swap));
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap();
+
+        expect(swapped).toEqual(expected);
+    });
+});
+
+describe('Testing process is reversable', () => {
+    const keys2swap = {
+        'PeachPie': 'poachedPear'
+    };
+
+    test('nested camalCase keys that are not in keys2swap are capitalised', () => {
+        data = {
+            'applePie': {
+                'poachedPear': 1
+            },
+            'pizzaPie': 2,
+        };
+        
+        expected = {
+            'ApplePie': {
+                'PeachPie': 1,
+            },
+            'PizzaPie': 2,
+        };
+
+        keySwapper = new KeySwapper(data, keys2swap);
+        swapped = keySwapper.swap(true);
+
+        expect(swapped).toEqual(expected);
+    });
+
+    test('keys that a reverse swapped not in keys2swap are capatalised', () => {
+        data = {
+            'applePie': 1,
+            'pizzaPie': 2,
+        };
+        
+        expected = {
+            'ApplePie': 1,
+            'PizzaPie': 2,
+        };
+
+        keySwapper = new KeySwapper(data, {});
+        swapped = keySwapper.swap(true);
+
+        expect(swapped).toEqual(expected);
     });
 });
